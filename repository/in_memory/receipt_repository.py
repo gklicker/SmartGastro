abde0451be7b6d831d9f2b91cb1f65d47e91f7c9
Andrew <a-y.yushkov@outlook.com>
2026-05-05 20:40:00 -0300

    feat: add repositories for foodtruck, ingredient, menu item, receipt and user

---
+++ repository/in_memory/receipt_repository.py
@@ -0,0 +1,32 @@
+from models.receipt import ReceiptStatus
+
+
+class ReceiptRepository:
+    def __init__(self):
+        self.__receipts = []
+
+    def save(self, receipt):
+        self.__receipts.append(receipt)
+        return receipt
+
+    def find_by_id(self, id):
+        for r in self.__receipts:
+            if r.get_id() == id:
+                return r
+        return None
+
+    def list_by_foodtruck(self, foodtruck_id):
+        return [r for r in self.__receipts if r.get_foodtruck_id() == foodtruck_id]
+
+    def list_by_event(self, event_id):
+        return [r for r in self.__receipts if r.get_event_id() == event_id]
+
+    def event_revenue(self, event_id):
+        return sum(
+            r.get_total()
+            for r in self.__receipts
+            if r.get_event_id() == event_id and r.get_status() == ReceiptStatus.CLOSED
+        )
+
+    def list_all(self):
+        return list(self.__receipts)
