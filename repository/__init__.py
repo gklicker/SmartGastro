51d230da173c7714820e619849baa8b035e6dffb
Andrew <a-y.yushkov@outlook.com>
2026-05-05 19:28:00 -0300

    feat: add repository, services layers and extend Evento model

---
+++ repository/__init__.py
@@ -0,0 +1,11 @@
+from repository.in_memory import (
+    ProductoRepository,
+    VentaRepository,
+    EventoRepository,
+    UserRepository,
+    IngredientRepository,
+    MenuItemRepository,
+    FoodtruckRepository,
+    EventRepository,
+    ReceiptRepository,
+)
